1311ec71744fb5b1506b1b61f49041f5775358dc
Dheeraj <samviswakarma5@gmail.com>
2019-04-07 17:19:51 +0530

    changing js

--- noteScript.js
+++ noteScript.js
@@ -1,19 +1,16 @@
 const icon = document.querySelectorAll("ul li p i");
-const ulist = document.getElementById("ul");
-const lists = document.querySelectorAll("ul li");
 const delBtn = document.querySelectorAll("delete-btn");
-
+const lists = document.querySelectorAll("ul li");
+const ulist = document.getElementById("ul");
 
 icon.forEach((del,index) =>{
-   del.addEventListener("click",function(){
-     ulist.removeChild(lists[index]); 
-    });
+   del.addEventListener("click",function deleteNote(){
+    ulist.removeChild(lists[index]);
+     });
 });
 
-lists.forEach((list,index)=>{
-    list.addEventListener("click",function(){
-        const typeNote = document.querySelectorAll("type-note")
-        // typeNote.contenteditable = true;
-        console.log(typeNote.getParent);
-    })
-})
+lists.forEach((edit,index) =>{
+   edit.addEventListener("click",function editNote(){
+        
+     });
+});
